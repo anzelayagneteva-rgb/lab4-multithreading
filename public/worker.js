f9ca0c231d36b57c40e89dbbbca0acfce646294d
anzelayagneteva-rgb <anzelayagneteva@gmail.com>
2026-06-12 02:24:36 +0400

    Refactor worker.js for modern syntax and readability

--- public/worker.js
+++ public/worker.js
@@ -1,8 +1,10 @@
-self.onmessage = function(event) {
-  var array = event.data.array;
-  var sum = 0;
-  for (var i = 0; i < array.length; i++) {
+self.onmessage = function (event) {
+  const array = event.data.array;
+  let sum = 0;
+
+  for (let i = 0; i < array.length; i++) {
     sum += array[i];
   }
-  self.postMessage({ sum: sum });
+
+  self.postMessage({ sum });
 };
